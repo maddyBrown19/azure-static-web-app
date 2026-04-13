68029cd2aec8ef30d5a92f72a43900ab99610263
Maddy Brown <brownm3@carleton.edu>
2026-04-13 14:56:44 -0500

    centered content and other styling changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
@@ -74,30 +74,36 @@ function App() {
   const handleMostStreamedSongClick = () => {
     const findArtist = artists.find(artist => artist.name === selectedArtist);
     if (findArtist) {
-      setMostStreamedSong(`${findArtist.name}'s most streamed song is ${findArtist.mostStreamedSong}`);
+      setMostStreamedSong(`${findArtist.name}'s most streamed song is "${findArtist.mostStreamedSong}"`);
     }
   };
 
   return (
-    <div>
-      <label style="paddingBottom: 20px">
-        <select name="selectedArtist" value={selectedArtist} onChange={handleSelectedArtistChange}>
-          <option value="">Choose an artist...</option>
-          {artists.map((artist) => (
-            <option key={artist.name} value={artist.name}>
-              {artist.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <Stack spacing={2} direction="row">
-        <Button variant="outlined" onClick={() => handleMonthlyListenerCountClick(selectedArtist)}>Get their monthly listener count</Button>
-        <Button variant="outlined" onClick={() => handleMostStreamedSongClick(selectedArtist)}>Get their most streamed song</Button>
-      </Stack>
-      <p></p>
-      <p>{monthlyListeners}</p>
-      <p>{mostStreamedSong}</p>
-    </div>
+    <span style={{fontFamily: "Monaco"}}>
+      <h1 style={{textAlign: "center", paddingTop: 20}}>Learn about Spotify artists</h1>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingBottom: 20 }}>
+        <label>
+          <select name="selectedArtist" value={selectedArtist} onChange={handleSelectedArtistChange}>
+            <option value="">Choose an artist...</option>
+            {artists.map((artist) => (
+              <option key={artist.name} value={artist.name}>
+                {artist.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Stack spacing={2} direction="row">
+          <Button variant="outlined" onClick={() => handleMonthlyListenerCountClick(selectedArtist)}>Monthly listener count</Button>
+          <Button variant="outlined" onClick={() => handleMostStreamedSongClick(selectedArtist)}>Most streamed song</Button>
+        </Stack>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <p>{monthlyListeners}</p>
+        <p>{mostStreamedSong}</p>
+      </div>
+    </span>
   );
 }
 
