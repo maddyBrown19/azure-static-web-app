c67d5b312f4cf8dd3536b16c220c5a7b94e26f26
Maddy Brown <brownm3@carleton.edu>
2026-04-27 15:25:25 -0500

    added new dataset and structured all the CSV data

--- src/main.go
+++ src/main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
+
+	//"fmt"
+	"log"
 	"net/http"
+	"os"
+	//"strings"
 )
 
+/*
 type artist struct {
 	Name             string `json:"name"`
 	MonthlyListeners int    `json:"monthlyListeners"`
 	MostStreamedSong string `json:"mostStreamedSong"`
 }
+*/
 
+type DataRow struct {
+	TrackID          string `json:"trackID"`
+	TrackName        string `json:"trackName"`
+	TrackNumber      string `json:"trackNumber"`
+	TrackPopularity  string `json:"trackPopularity"`
+	Explicit         string `json:"explicit"`
+	ArtistName       string `json:"artistName"`
+	ArtistPopularity string `json:"artistPopularity"`
+	ArtistFollowers  string `json:"artistFollowers"`
+	ArtistGenres     string `json:"artistGenres"`
+	AlbumID          string `json:"albumID"`
+	AlbumName        string `json:"albumName"`
+	AlbumReleaseDate string `json:"albumReleaseDate"`
+	AlbumTotalTracks string `json:"albumTotalTracks"`
+	AlbumType        string `json:"albumType"`
+	TrackDurationMin string `json:"trackDurationMin"`
+}
+
+/*
 var artists = []artist{
 	{Name: "Maggie Rogers", MonthlyListeners: 7089646, MostStreamedSong: "Dawns (feat. Maggie Rogers)"},
 	{Name: "Lorde", MonthlyListeners: 27123369, MostStreamedSong: "Ribs"},
@@ -22,18 +49,86 @@ var artists = []artist{
 	{Name: "Billie Eilish", MonthlyListeners: 84774525, MostStreamedSong: "BIRDS OF A FEATHER"},
 	{Name: "Gracie Abrams", MonthlyListeners: 34874334, MostStreamedSong: "That's So True"},
 	{Name: "Ethel Cain", MonthlyListeners: 3442765, MostStreamedSong: "Crush"},
+} */
+
+func parseSpotifyData(data [][]string) []DataRow {
+	var spotifyData []DataRow
+	for i, line := range data {
+		if i == 0 || len(line) < 15 {
+			continue
+		}
+		var row DataRow
+		for j, value := range line {
+			//cleanValue := strings.ReplaceAll(value, ",", "")
+			switch j {
+			case 0:
+				row.TrackID = value
+			case 1:
+				row.TrackName = value
+			case 2:
+				row.TrackNumber = value
+			case 3:
+				row.TrackPopularity = value
+			case 4:
+				row.Explicit = value
+			case 5:
+				row.ArtistName = value
+			case 6:
+				row.ArtistPopularity = value
+			case 7:
+				row.ArtistFollowers = value
+			case 8:
+				row.ArtistGenres = value
+			case 9:
+				row.AlbumID = value
+			case 10:
+				row.AlbumName = value
+			case 11:
+				row.AlbumReleaseDate = value
+			case 12:
+				row.AlbumTotalTracks = value
+			case 13:
+				row.AlbumType = value
+			case 14:
+				row.TrackDurationMin = value
+			}
+		}
+		spotifyData = append(spotifyData, row)
+	}
+	return spotifyData
 }
+
+func getSpotifyData() []DataRow {
+	file, e := os.Open("spotify_data.csv")
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.LazyQuotes = true
+	reader.FieldsPerRecord = -1
+	data, e := reader.ReadAll()
+	if e != nil {
+		log.Fatal(e)
+	}
+	parsedSpotifyData := parseSpotifyData(data)
+	return parsedSpotifyData
+}
+
+var data []DataRow = getSpotifyData()
 
 func getArtistNames(w http.ResponseWriter, r *http.Request) {
 	var names []string
-	for _, artist := range artists {
-		names = append(names, artist.Name)
+	for _, dataRow := range data {
+		names = append(names, dataRow.ArtistName)
 	}
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(names)
 }
+
+/*
 
 func getSelectedDataByArtist(w http.ResponseWriter, r *http.Request) {
 	name := r.PathValue("name")
@@ -61,10 +156,11 @@ func getSelectedDataByArtist(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+*/
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /artistNames", getArtistNames)
-	mux.HandleFunc("GET /artist/{name}/{data}", getSelectedDataByArtist)
+	//mux.HandleFunc("GET /artist/{name}/{data}", getSelectedDataByArtist)
 	http.ListenAndServe(":8080", mux)
 }
